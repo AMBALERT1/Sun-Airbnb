635c1de2a1c7c3d0da7266c75cd16537833685e9
Amber Jackson <ajjackson921@icloud.com>
2024-05-22 10:58:32 -0400

    adding images

---
+++ backend/db/seeders/20240522125843-seed-spots.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.bulkInsert('Spots', [
+    {
+      ownerId: 1,
+      address: '124 Main St',
+      city: 'Anytown',
+      state: 'CA',
+      country: 'USA',
+      lat: 37.7749,
+      lng: -122.4194,
+      name: 'Wonderful Spot',
+      description: 'A beautiful spot in the hear of the city.',
+      price: 100.00
+      previewImage: ,
+      avgRating: 5.5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+  }
+};
