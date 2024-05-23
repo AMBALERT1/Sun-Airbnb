e113a814025bf02af24b397d0029eae17cfdfea1
Amber Jackson <ajjackson921@icloud.com>
2024-05-23 19:29:02 -0400

    added two more seed data to the seeder file, updated my spots migration

--- backend/db/migrations/02-spot.js
+++ backend/db/migrations/02-spot.js
@@ -1,4 +1,13 @@
 'use strict';
+
+const { options } = require('../../routes');
+
+let options = {};
+options.tablename = 'Spots'
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -87,7 +96,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
